5b6133df49361ae0fdad5e9b14ab7c487356b7f3
jazielloureiro <jazielloureiro17@gmail.com>
2022-11-02 10:51:25 -0300

    Add resources by process calculation

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 #!/bin/python3
+
 from argparse import ArgumentParser
+from functools import reduce
 
 class MapFile:
     def __init__(self, filepath):
         with open(filepath) as file:
             line = file.readline().replace("\n", "")
-            [self.process_qty, self.resources_qty] = line.split(" ")
+            [self.process_qty, self.resources_qty] = [int(i) for i in line.split(" ") if i.isdecimal()]
 
             file.readline()
 
             line = file.readline().replace("\n", "")
-            self.total_resources = line.split(" ")
+            self.total_resources = [int(i) for i in line.split(" ") if i.isdecimal()]
 
             file.readline()
 
             line = file.readline().replace("\n", "")
-            self.available_resources = line.split(" ")
+            self.available_resources = [int(i) for i in line.split(" ") if i.isdecimal()]
 
             file.readline()
 
@@ -23,7 +25,7 @@ class MapFile:
 
             for i in range(int(self.process_qty)):
                 line = file.readline().replace("\n", "")
-                self.allocated_resources += [line.split(" ")]
+                self.allocated_resources += [[int(r) for r in line.split(" ") if r.isdecimal()]]
 
             file.readline()
 
@@ -31,7 +33,35 @@ class MapFile:
 
             for i in range(int(self.process_qty)):
                 line = file.readline().replace("\n", "")
-                self.request_resources += [line.split(" ")]
+                self.request_resources += [[int(r) for r in line.split(" ") if r.isdecimal()]]
+
+    def calc_resources_by_process(self):
+        self.resources_by_process = []
+
+        for i in range(self.process_qty):
+            total_resources = reduce(lambda acc, r: acc + r, self.request_resources[i])
+
+            self.resources_by_process.append({
+                "id": i,
+                "total": total_resources
+            })
+
+        self.resources_by_process = sorted(self.resources_by_process, key=lambda r: r["total"], reverse=True)
+
+def main(filepath):
+    file = MapFile(filepath)
+    file.calc_resources_by_process()
+
+    print(file.__dict__)
+
+def can_process_be_processed(file, process_id):
+    for i in range(file.resources_qty):
+        all_resources = file.available_resources[i] + file.allocated_resources[process_id][i]
+
+        if all_resources < file.request_resources[process_id][i]:
+            return False
+
+    return True
 
 if __name__ == "__main__":
 
@@ -41,5 +71,4 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
 
-    print(MapFile(args.filepath).__dict__)
-
+    main(args.filepath)
